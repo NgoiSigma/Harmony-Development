871c161cce090c9f8d5f44527d51cb25669ee78f
NOVEYA <ngoisigma@gmail.com>
2026-02-07 22:40:44 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0-NGI-Alpha" // Версия NGI Alpha
+        versionName = "1.0-NGOI-Alpha" // Версия NGOI Alpha
     }
     // ... rest of config
 }
